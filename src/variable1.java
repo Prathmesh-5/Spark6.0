d50b17820b01f6515a518579028bf00a9702dfe9
prathmesh-5 <prathmesh321vns@gmail.com>
2026-06-25 00:23:54 +0530

    done

---
+++ src/variable1.java
@@ -0,0 +1,9 @@
+public class variable1 {
+    public static void main(String[] args) {
+        int num=20;
+        String name="Prathameshh";
+        System.out.println(num);
+        System.out.println(name);
+    }
+
+}
