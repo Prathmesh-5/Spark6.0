1a9f523d1f33ac7001279f9e4f490c0023e8784c
prathmesh-5 <prathmesh321vns@gmail.com>
2026-06-25 18:17:00 +0530

    done

---
+++ src/DataType.java
@@ -0,0 +1,57 @@
+public class DataType {
+    public static void main(String[] args){
+        // int data type
+        int age=14,marks=95,student=50;
+        System.out.println("Age : "+age);
+        System.out.println("Marks : "+marks);
+        System.out.println("Student : "+student);
+        if(age>10){
+            System.out.println("hi");
+        }
+
+        //------------------------------------------
+
+        // double data type
+        float pi=3.14f;
+        double salary=9899989.982;
+
+        System.out.println("pi = "+pi);
+        System.out.println("salary = "+salary);
+
+        //-------------------------------------------------
+
+        // character data type
+        char grade='A';
+        System.out.println("grade = "+grade);
+
+        //---------------------------------------------------
+
+        // boolean data type
+        boolean isAdult=true;
+        System.out.println("is Adult = "+isAdult);
+
+        //--------------------------------------------------
+
+        // String data type
+        String name="Raj";
+        System.out.println("name = "+name);
+
+        //---------------------------------------------------
+
+        // more types on integer
+        byte smallNum=100;
+        short mediumNum=800;
+        int normalNum=4200;
+        long bigNum=5776358185132L;
+
+        System.out.println("smallNum = "+smallNum);
+        System.out.println("mediumNum = "+mediumNum);
+        System.out.println("normalNum = "+normalNum);
+        System.out.println("bigNum = "+bigNum);
+
+        //---------------------------------------------------
+
+
+
+    }
+}
