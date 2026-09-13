68d409ef438a518b6651c9267c3ce8e1c0abae37
prathmesh-5 <prathmesh321vns@gmail.com>
2026-09-13 14:47:24 +0530

    done

---
+++ src/ExceptionHandling/Overview/TestOverview.java
@@ -0,0 +1,12 @@
+package ExceptionHandling.Overview;
+
+public class TestOverview {
+    public static void main(String[] args) {
+        int a=10;
+        int b=0;
+
+        System.out.println("addition = "+(a+b));
+        System.out.println("division = "+(a/b));
+
+    }
+}
