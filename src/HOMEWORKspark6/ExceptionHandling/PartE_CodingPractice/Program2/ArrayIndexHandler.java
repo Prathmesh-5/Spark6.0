72280efc7437fbea8b90a12e0ba656e96b649777
prathmesh-5 <prathmesh321vns@gmail.com>
2026-09-15 16:50:12 +0530

    done

--- src/HOMEWORKspark6/ExceptionHandling/PartE_CodingPractice/Program2/ArrayIndexHandler.java
+++ src/HOMEWORKspark6/ExceptionHandling/PartE_CodingPractice/Program2/ArrayIndexHandler.java
@@ -6,14 +6,18 @@ public class ArrayIndexHandler {
         Scanner sc = new Scanner(System.in);
         int[] arr={10,22,42,11,85};
 
-        System.out.println("enter index : ");
-        int idx=sc.nextInt();
+
         int element=-1;
         try{
+            System.out.println("enter index : ");
+            int idx = sc.nextInt();
             element=arr[idx];
         }
         catch(ArrayIndexOutOfBoundsException e){
-            System.out.println("ye index out of bound hai");
+            System.out.println("ye index out of bound hai !");
+        }
+        catch(InputMismatchException e){
+            System.out.println("invalid data type ka input diye ho !");
         }
 
         System.out.println("The element is : "+element);
